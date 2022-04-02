57953b72c6fd6971a9664e2bb3be8a061cb8e09c
paulsandipan5 <paulsandipan5@gmail.com>
2022-04-02 09:39:34 +0530

    Problem 1

---
+++ Assignment 4_Problem_1/Solution.py
@@ -0,0 +1,3 @@
+# Write a Python program to create a lambda function that adds 25 to a given number passed in as an argument.
+
+print("The output:",(lambda x : x + 25)(int(input("Enter the number:"))))
